a8dce107e3fa65371670e72537abc480009d438c
Chris Amos <cyamos2@yahoo.com>
2024-02-01 14:12:51 -0500

    fixed the server

--- src/server.js
+++ src/server.js
@@ -1,22 +1,16 @@
-// server.js
+// src/server.js
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.get('/guineaPigCare.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/guineaPigCare.html'));
-});
-
-app.get('/guineaPigHistory.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/guineaPigHistory.html'));
+// Catch-all route to serve 'index.html'
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(port, () => {
